8c85516d4473e33ffa304538688480d502d8fee8
Muskan Shaikh <shaikhitsmuskan@gmail.com>
2023-04-28 17:54:48 +0530

    [Muskan] Added : Main method for read and write persons contact from Json file.

--- Program.cs
+++ Program.cs
@@ -10,22 +10,37 @@ namespace AddressBook_ThirdPartyLibrary
     {
         static void Main(string[] args)
         {
-            AddressBook addressBook = new AddressBook();
+            //AddressBook addressBook = new AddressBook();
 
-            addressBook.AddContact(new Person { Name = "John Doe", Email = "john.doe@example.com", Phone = "555-1234" });
-            addressBook.AddContact(new Person { Name = "Jane Smith", Email = "jane.smith@example.com", Phone = "555-5678" });
+            //addressBook.AddContact(new Person { Name = "John Doe", Email = "john.doe@example.com", Phone = "555-1234" });
+            //addressBook.AddContact(new Person { Name = "Jane Smith", Email = "jane.smith@example.com", Phone = "555-5678" });
 
-            string fileName = "contact.csv";
-            string filePath = @"C:\Users\91997\source\repos\AddressBook_ThirdPartyLibrary\contact.csv";
+            //string fileName = "contact.csv";
+            //string filePath = @"C:\Users\91997\source\repos\AddressBook_ThirdPartyLibrary\contact.csv";
 
-            addressBook.WriteContacts(filePath + fileName);
+            //addressBook.WriteContacts(filePath + fileName);
 
-            addressBook.ReadContacts(filePath + fileName);
+            //addressBook.ReadContacts(filePath + fileName);
 
-            foreach (Person person in addressBook.Contacts)
-            {
-                System.Console.WriteLine(person.Name + " - " + person.Email + " - " + person.Phone);
-            }
+            //foreach (Person person in addressBook.Contacts)
+            //{
+            //    System.Console.WriteLine(person.Name + " - " + person.Email + " - " + person.Phone);
+            //}
+
+
+            AddressBookJSON addressBookJSON = new AddressBookJSON();
+
+            string fileName = "Contact.Json";
+            string filePath = @"C:\\Users\\91997\\source\\repos\\AddressBook_ThirdPartyLibrary\\Contact.json";
+
+            addressBookJSON.WriteContacts(filePath + fileName);
+            addressBookJSON.ReadContacts(filePath + fileName);
+
+            addressBookJSON.AddContact(new Person { Name = "Muskan Shaikh", Email="mushk1111shaikh@gmail.com", Phone = "9975001230" });
+            addressBookJSON.DisplayContacts();
+
+
+
         }
     }
 }
